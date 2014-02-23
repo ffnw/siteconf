ad98616392bcd99fb53d7e4aa0ef761b3f015830
Bjoern Franke <bjo@nord-west.org>
2014-02-23 21:49:28 +0100

    created siteconf for FFOL

---
+++ site.mk
@@ -0,0 +1,16 @@
+GLUON_SITE_PACKAGES := \
+	gluon-config-mode \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-mesh-batman-adv \
+	gluon-mesh-vpn-fastd \
+	gluon-autoupdater \
+	gluon-alfred \
+	iputils-ping6 \
+	iwinfo \
+	iptables \
+	kmod-ipt-nathelper \
+	firewall \
+	haveged \
+	gluon-status-page \
+	gluon-next-node
