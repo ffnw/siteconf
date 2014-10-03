af380d51f1a629a5721dc28afead6cb445bc7055
bioxz <bioxz@stderr.cc>
2014-10-03 21:03:39 +0200

    Reverted site.mk to work with gluon 2014.3

--- site.mk
+++ site.mk
@@ -3,22 +3,17 @@ GLUON_SITE_PACKAGES := \
 	gluon-announced \
 	gluon-ath9k-workaround \
 	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
+	gluon-config-mode
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-next-node \
-	gluon-mesh-batman-adv-14 \
+	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
-	gluon-luci-private-wifi \
 	iwinfo \
 	iptables \
 	haveged \
