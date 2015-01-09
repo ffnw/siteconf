41cd5bdb2fb435bf889574bf4ee66776092f3f04
Jan-Tarek Butt <buttjantarek@googlemail.com>
2015-01-09 11:25:57 +0100

    Set version number of 0.5.4

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	ffnw-configurator \
 	ffnw-nodewatcher
 
-DEFAULT_GLUON_RELEASE := 0.5.3.99+0$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.5.4+0$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
